9109d3d8207a1de91e3bc7833101f0a627df40ec
snehamaria-tom <chn19cs113@gmail.com>
2020-10-24 22:28:37 +0530

    Java program to add two numbers

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class add{ 
+	public static void main(String[] args) {
+	Scanner read=new Scanner(System.in);
+	int a,b;
+	System.out.println("Enter the two numbers");
+	a=read.nextInt();
+	b=read.nextInt();
+	System.out.println("Sum="+(a+b));
+}
+}
